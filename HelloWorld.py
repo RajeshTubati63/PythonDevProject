e107fa32c99f93cdaec0e33ec31740e5d24476e7
Rajesh Babu Tubati <rajeshbabu.tubati@pridevel.com>
2017-03-10 21:09:46 +0530

    Added welcome route

--- HelloWorld.py
+++ HelloWorld.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def Helloworld():
     return '<h1>Hello world Welcome to Heroku !!!</h1>'
 
+@app.route('/welcome')
+def Welcome():
+    return '<h1>Welcome to My world !!! </h1>'
+
 if __name__ == '__main__':
     app.debug = True
     app.run(host = '0.0.0.0', port = 8080)
