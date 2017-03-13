59079272efb2b1207268270b5033f5c130f7ff25
Rajesh Babu Tubati <rajeshbabu.tubati@pridevel.com>
2017-03-13 15:08:44 +0530

    Created new html files for add and edit items

--- HelloWorld.py
+++ HelloWorld.py
@@ -12,14 +12,10 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind = engine)
 session = DBSession()
 
-@app.route('/')
 def Helloworld():
     print ("python 3.5")
-    createRestaurantRecord(itemName)
-
+#     createRestaurantRecord(itemName)
     return '<h1>Hello world Welcome to Heroku !!!</h1>'
-
-
 @app.route('/welcome')
 def Welcome():
     return '<h1>Welcome to My world !!! </h1>'
@@ -30,26 +26,57 @@ def FindRestaurantMenuByID(restaurant_id):
     items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
     return render_template('menu.html',restaurant = restaurant,items = items)
 
+@app.route('/')
+@app.route('/index/')
+@app.route('/restaurants/')
+def GetRestaurants():
+    restaurants = session.query(Restaurant).all()
+    return render_template('index.html',restaurants = restaurants)
 
+#Task 1: Create route for newMenuItem function here
+@app.route('/restaurant/<int:restaurant_id>/new/', methods = ['GET','POST'])
+def newMenuItem(restaurant_id):
+    if(request.method == 'POST'):
+        newItem  = MenuItem(name = request.form['name'],restaurant_id = restaurant_id )
+        session.add(newItem)
+        session.commit()
+        return redirect(url_for('FindRestaurantMenuByID',restaurant_id = restaurant_id))
+    else:
+        return render_template('newmenuitem.html',restaurant_id = restaurant_id)
+    
 
-def createRestaurantRecord(itemName):
-# Insert 
-    myFirstRestaurant = Restaurant(name = "Pizza Palace")
-    session.add(myFirstRestaurant)
-    session.commit()  
-
-    menuItem = MenuItem(name = itemName, course = 'Pizzas', description = "This is about biriyani's here", restaurant_id = myFirstRestaurant.id)
-    session.add(menuItem)
-    session.commit()
-#Read Menu items based on Restaurant id
-    allRows = session.query(MenuItem).all()
-    for item in allRows:
-        print (item.id)
-        print (item.name)
-#Update 
-#     oneRow.name = "Chicken Burger"
-#     session.add(oneRow)
-#     session.commit()
+#Task 2: Create route for editMenuItem function here
+@app.route('/restaurant/<int:restaurant_id>/<int:MenuID>/edit/', methods = ['GET', 'POST'])
+def editMenuItem(restaurant_id, MenuID):
+    editedItem = session.query(MenuItem).filter_by(id = MenuID).one()
+    if request.method == 'POST':
+        if request.form['name']:
+            editedItem.name = request.form['name']
+        session.add(editedItem)
+        session.commit()
+        return redirect(url_for('FindRestaurantMenuByID', restaurant_id = restaurant_id))
+    else:
+        
+        #USE THE RENDER_TEMPLATE FUNCTION BELOW TO SEE THE VARIABLES YOU SHOULD USE IN YOUR EDITMENUITEM TEMPLATE
+        return render_template('editmenuitem.html', restaurant_id = restaurant_id, MenuID = MenuID, item = editedItem)
+    
+@app.route('/restaurant/new/',methods = ['GET','POST'])   
+def createNewResturant():
+    if request.method == 'POST':
+        
+        name = request.form['name']
+        myRestaurant = Restaurant(name = name)
+        session.add(myRestaurant)
+        session.commit()
+        return redirect(url_for('GetRestaurants'))
+    else:
+        return render_template('newrestaurant.html')
+    
+#Task 3: Create a route for deleteMenuItem function here
+@app.route('/restaurant/<int:restaurant_id>/<int:MenuID>/delete/')
+def deleteMenuItem(restaurant_id, MenuID):
+    return "page to delete a new menu item. Task 3 complete!"
+         
 
 
 if __name__ == '__main__':
